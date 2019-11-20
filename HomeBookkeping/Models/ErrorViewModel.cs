6f4d4058c7cef558748d43387321b94406719643
Wlad_Gorbunov_1997@outlook.com <Wlad_Gorbunov_1997@outlook.com>
2019-11-20 22:22:36 +0500

    Added HomeBookkeping.Models.User

---
+++ HomeBookkeping/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeBookkeping.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
